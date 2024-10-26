61a14500611a98136ec46a668906b7d1c9724406
cidibuyijiuliu <2503991005@qq.com>
2024-10-26 09:27:14 +0800

    logindata

--- property/src/components/Echarts.vue
+++ property/src/components/Echarts.vue
@@ -1,9 +1,485 @@
 <template>
-  <div></div>
+  <el-row :gutter="20" style="margin: 0">
+    <!-- 上方滚动条 -->
+    <el-col :span="24">
+      <div
+        class="grid-content bg-purple-dark"
+        style="height: 56px; background-color: #ccc"
+      ></div>
+    </el-col>
+    <!-- 下方四个卡片 -->
+    <el-col :span="6">
+      <div
+        class="grid-content bg-purple"
+        style="
+          background-color: #ccc;
+          height: 100px;
+          border-radius: 8px;
+          margin: 10px;
+        "
+      >
+        <i class="el-icon-user-solid"></i>
+        <span style="font-size: 32px; font-weight: bold">{{ UsersData }}</span>
+        <span>系统用户数</span>
+      </div>
+    </el-col>
+    <el-col :span="6">
+      <div
+        class="grid-content bg-purple"
+        style="
+          background-color: #ccc;
+          height: 100px;
+          border-radius: 8px;
+          margin: 10px;
+        "
+      >
+        <i class="el-icon-user-solid"></i>
+        <span style="font-size: 32px; font-weight: bold">{{ payMessage }}</span>
+        <span>计费订单数量</span>
+      </div>
+    </el-col>
+    <el-col :span="6">
+      <div
+        class="grid-content bg-purple"
+        style="
+          background-color: #ccc;
+          height: 100px;
+          border-radius: 8px;
+          margin: 10px;
+        "
+      >
+        <i class="el-icon-user-solid"></i>
+        <span style="font-size: 32px; font-weight: bold">{{ ParkOrder }}</span>
+        <span>车位数量</span>
+      </div>
+    </el-col>
+    <el-col :span="6">
+      <div
+        class="grid-content bg-purple"
+        style="
+          background-color: #ccc;
+          height: 100px;
+          border-radius: 8px;
+          margin: 10px;
+        "
+      >
+        <i class="el-icon-user-solid"></i>
+        <span style="font-size: 32px; font-weight: bold">{{ FeedBack }}</span>
+        <span>用户反馈数量</span>
+      </div>
+    </el-col>
+    <!-- 小区分布统计 -->
+    <el-col :span="8">
+      <div
+        class="grid-content bg-purple"
+        style="
+          background-color: #ccc;
+          height: 300px;
+          border-radius: 8px;
+          margin: 10px;
+        "
+      ></div>
+    </el-col>
+    <el-col :span="8">
+      <div
+        class="grid-content bg-purple"
+        style="
+          background-color: #ccc;
+          height: 300px;
+          border-radius: 8px;
+          margin: 10px;
+        "
+        id="Community"
+      ></div>
+    </el-col>
+    <el-col :span="8">
+      <div
+        class="grid-content bg-purple"
+        style="
+          background-color: #ccc;
+          height: 300px;
+          border-radius: 8px;
+          margin: 10px;
+        "
+      ></div>
+    </el-col>
+  </el-row>
 </template>
 
 <script>
-export default {}
+import * as echarts from "echarts";
+
+export default {
+  data() {
+    return {
+      UsersData: "",
+      payMessage: "",
+      ParkOrder: "",
+      FeedBack: "",
+      chartData: {
+        a1: "",
+        a2: "",
+        a3: "",
+        b1: "",
+        b2: "",
+        b3: "",
+        c1: "",
+        c2: "",
+        c3: "",
+        d1: "",
+        d2: "",
+        d3: "",
+      },
+    };
+  },
+  mounted() {
+    this.getUsersData();
+    this.getpayMessage();
+    this.getpayParkOrder();
+    this.getFeedBack();
+    this.getEchartsDataa1();
+    this.getEchartsDataa2();
+    this.getEchartsDataa3();
+    this.getEchartsDatab1();
+    this.getEchartsDatab2();
+    this.getEchartsDatab3();
+    this.getEchartsDatac1();
+    this.getEchartsDatac2();
+    this.getEchartsDatac3();
+    this.getEchartsDatad1();
+    this.getEchartsDatad2();
+    this.getEchartsDatad3();
+    this.initEcharts(); // 初始化 ECharts
+    //调用a1的数据
+  },
+  methods: {
+    getUsersData() {
+      this.axios
+        .get(
+          "http://community.byesame.com/users/getUsersData?currPage=0&pageNum=4",
+          {
+            headers: {
+              token: sessionStorage.getItem("token"),
+            },
+          }
+        )
+        .then((res) => {
+          this.UsersData = res.data.total;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    getpayMessage() {
+      this.axios
+        .get(
+          "http://community.byesame.com/cost/getPayMessage?currPage=0&pageNum=4",
+          {
+            headers: {
+              token: sessionStorage.getItem("token"),
+            },
+          }
+        )
+        .then((res) => {
+          this.payMessage = res.data.total;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    getpayParkOrder() {
+      this.axios
+        .get(
+          "http://community.byesame.com/parking/getParkOrder?currPage=0&pageNum=4",
+          {
+            headers: {
+              token: sessionStorage.getItem("token"),
+            },
+          }
+        )
+        .then((res) => {
+          this.ParkOrder = res.data.total;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    getFeedBack() {
+      this.axios
+        .get(
+          "http://community.byesame.com/poster/getFeedBack?currPage=0&pageNum=4",
+          {
+            headers: {
+              token: sessionStorage.getItem("token"),
+            },
+          }
+        )
+        .then((res) => {
+          this.FeedBack = res.data.total;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    // 初始化Echarts
+    initEcharts() {
+      const chartDom = document.getElementById("Community");
+      const myChart = echarts.init(chartDom);
+      const option = {
+        legend: {},
+        tooltip: {},
+        dataset: {
+          source: [
+            ["product", "车位数", "已分配量", "已缴费"],
+            ["A区", this.chartData.a1, this.chartData.a2, this.chartData.a3],
+            ["B区", this.chartData.b1, this.chartData.b2, this.chartData.b3],
+            ["C区", this.chartData.c1, this.chartData.c2, this.chartData.c3],
+            ["D区", this.chartData.d1, this.chartData.d2, this.chartData.d3],
+          ],
+        },
+        xAxis: { type: "category" },
+        yAxis: {},
+        series: [{ type: "bar" }, { type: "bar" }, { type: "bar" }],
+      };
+
+      option && myChart.setOption(option);
+    },
+    // 调用Echarts中的动态数据
+    getEchartsDataa1() {
+      this.axios
+        .get(
+          "http://community.byesame.com/parking/getParkData?parkname=A&currPage=0&pageNum=4",
+          {
+            headers: {
+              token: sessionStorage.getItem("token"),
+            },
+          }
+        )
+        .then((res) => {
+          console.log(res.data.total);
+          this.chartData.a1 = res.data.total;
+          this.initEcharts();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    getEchartsDataa2() {
+      this.axios
+        .get(
+          "http://community.byesame.com/parking/getParkData?parkname=A&parkstatus=1&currPage=0&pageNum=4",
+          {
+            headers: {
+              token: sessionStorage.getItem("token"),
+            },
+          }
+        )
+        .then((res) => {
+          console.log(res.data.total);
+          this.chartData.a2 = res.data.total;
+          this.initEcharts();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    getEchartsDataa3() {
+      this.axios
+        .get(
+          "http://community.byesame.com/parking/getParkOrder?parkname=A&orderstatus=1&currPage=0&pageNum=4",
+          {
+            headers: {
+              token: sessionStorage.getItem("token"),
+            },
+          }
+        )
+        .then((res) => {
+          console.log(res.data.total);
+          this.chartData.a3 = res.data.total;
+          this.initEcharts();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    getEchartsDatab1() {
+      this.axios
+        .get(
+          "http://community.byesame.com/parking/getParkData?parkname=B&currPage=0&pageNum=4",
+          {
+            headers: {
+              token: sessionStorage.getItem("token"),
+            },
+          }
+        )
+        .then((res) => {
+          console.log(res.data.total);
+          this.chartData.b1 = res.data.total;
+          this.initEcharts();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    getEchartsDatab2() {
+      this.axios
+        .get(
+          "http://community.byesame.com/parking/getParkData?parkname=B&parkstatus=1&currPage=0&pageNum=4",
+          {
+            headers: {
+              token: sessionStorage.getItem("token"),
+            },
+          }
+        )
+        .then((res) => {
+          console.log(res.data.total);
+          this.chartData.b2 = res.data.total;
+          this.initEcharts();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    getEchartsDatab3() {
+      this.axios
+        .get(
+          "http://community.byesame.com/parking/getParkOrder?parkname=B&orderstatus=1&currPage=0&pageNum=4",
+          {
+            headers: {
+              token: sessionStorage.getItem("token"),
+            },
+          }
+        )
+        .then((res) => {
+          console.log(res.data.total);
+          this.chartData.b3 = res.data.total;
+          this.initEcharts();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    getEchartsDatac1() {
+      this.axios
+        .get(
+          "http://community.byesame.com/parking/getParkData?parkname=C&currPage=0&pageNum=4",
+          {
+            headers: {
+              token: sessionStorage.getItem("token"),
+            },
+          }
+        )
+        .then((res) => {
+          console.log(res.data.total);
+          this.chartData.c1 = res.data.total;
+          this.initEcharts();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    getEchartsDatac2() {
+      this.axios
+        .get(
+          "http://community.byesame.com/parking/getParkData?parkname=C&currPage=0&pageNum=4",
+          {
+            headers: {
+              token: sessionStorage.getItem("token"),
+            },
+          }
+        )
+        .then((res) => {
+          console.log(res.data.total);
+          this.chartData.c2 = res.data.total;
+          this.initEcharts();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    getEchartsDatac3() {
+      this.axios
+        .get(
+          "http://community.byesame.com/parking/getParkOrder?parkname=C&orderstatus=1&currPage=0&pageNum=4",
+          {
+            headers: {
+              token: sessionStorage.getItem("token"),
+            },
+          }
+        )
+        .then((res) => {
+          console.log(res.data.total);
+          this.chartData.c3 = res.data.total;
+          this.initEcharts();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    getEchartsDatad1() {
+      this.axios
+        .get(
+          "http://community.byesame.com/parking/getParkData?parkname=D&currPage=0&pageNum=4",
+          {
+            headers: {
+              token: sessionStorage.getItem("token"),
+            },
+          }
+        )
+        .then((res) => {
+          console.log(res.data.total);
+          this.chartData.d1 = res.data.total;
+          this.initEcharts();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    getEchartsDatad2() {
+      this.axios
+        .get(
+          "http://community.byesame.com/parking/getParkData?parkname=D&parkstatus=1&currPage=0&pageNum=4",
+          {
+            headers: {
+              token: sessionStorage.getItem("token"),
+            },
+          }
+        )
+        .then((res) => {
+          console.log(res.data.total);
+          this.chartData.d2 = res.data.total;
+          this.initEcharts();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    getEchartsDatad3() {
+      this.axios
+        .get(
+          "http://community.byesame.com/parking/getParkOrder?parkname=D&orderstatus=1&currPage=0&pageNum=4",
+          {
+            headers: {
+              token: sessionStorage.getItem("token"),
+            },
+          }
+        )
+        .then((res) => {
+          console.log(res.data.total);
+          this.chartData.d3 = res.data.total;
+          this.initEcharts();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  },
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+#Community {
+  height: 300px; /* 确保有高度 */
+  width: 100%; /* 确保有宽度 */
+}
+</style>
--- property/src/components/LoginData.vue
+++ property/src/components/LoginData.vue
@@ -1,9 +1,214 @@
 <template>
-  <div>123</div>
+  <div class="box">
+    <div class="top">
+      <el-input v-model="username" placeholder="用户账号" clearable style="width: 20%;"></el-input>
+      <el-select v-model="type" placeholder="角色类型" style="width: 20%; margin:0 10px;">
+        <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value" clearable>
+        </el-option>
+      </el-select>
+      <el-date-picker v-model="value2" type="daterange" align="right" unlink-panels range-separator="至"
+        start-placeholder="开始日期" end-placeholder="结束日期" :picker-options="pickerOptions" style="width: 25%;">
+      </el-date-picker>
+      <el-button type="primary" icon="el-icon-search" style="width: 8%; font-size: 14px; margin-left: 10px;"
+        @click="search">查询</el-button>
+    </div>
+    <div class="tab">
+      <el-tabs type="border-card">
+        <el-tab-pane label="登录日志">
+          <el-table :data="tableData" border style="width: 100%">
+
+            <el-table-column type="index" label="序号" width="80">
+            </el-table-column>
+            <el-table-column prop="username" label="登陆账户" min-width="226">
+            </el-table-column>
+            <el-table-column prop="type" label="角色类型" min-width="223">
+              {{ tableData.type === admin ? '管理员' : '普通用户' }}
+            </el-table-column>
+            <el-table-column prop="email" label="邮箱" min-width="297">
+            </el-table-column>
+            <el-table-column prop="createtime" label="登录时间" min-width="446">
+            </el-table-column>
+            <el-table-column label="操作" min-width="163">
+              <!-- <template slot-scope="scope"> -->
+              <template>
+                <!-- <el-button style="background-color: red;color: white;"
+                  @click="handleDetele(scope.row.id)">删除记录</el-button> -->
+                <el-button style="background-color: red;color: white;"
+                 >删除记录</el-button>
+              </template>
+            </el-table-column>
+
+          </el-table>
+          <!-- 分页 -->
+          <div class="block" style="text-align: right ; margin-top: 20px;">
+            <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange"
+              :current-page="currentPage4" :page-sizes="[5, 9, 20, 50, 100]" :page-size="9"
+              layout="total, sizes, prev, pager, next,jumper" :total="total">
+            </el-pagination>
+          </div>
+        </el-tab-pane>
+      </el-tabs>
+    </div>
+  </div>
 </template>
-
 <script>
-export default {}
-</script>
+export default {
+  data() {
+    return {
+      id: '',
+      username: '',
+      type: '',
+      startDate: '',
+      endDate: '',
+      currPage: 1,
+      pageNum: 9,
+      total: null,
+      currentPage4: 1,
+      currentPage: 1,
+      admin: '',
+      tableData: [],
+      createtime: '',
+      value2: '',
+      parameters: {
+        username: "",
+        type: "",
+        startDate: "",
+        endDate: "",
+        currPage: 1,
+        pageNum: 9,
+      },
+      options: [{
+        value: '1',
+        label: '管理员'
+      }, {
+        value: '2',
+        label: '普通用户'
+      },],
+      pickerOptions: {
+        shortcuts: [{
+          text: '最近一周',
+          onClick(picker) {
+            const end = new Date();
+            const start = new Date();
+            start.setTime(start.getTime() - 3600 * 1000 * 24 * 7);
+            picker.$emit('pick', [start, end]);
+          }
+        }, {
+          text: '最近一个月',
+          onClick(picker) {
+            const end = new Date();
+            const start = new Date();
+            start.setTime(start.getTime() - 3600 * 1000 * 24 * 30);
+            picker.$emit('pick', [start, end]);
+          }
+        }, {
+          text: '最近三个月',
+          onClick(picker) {
+            const end = new Date();
+            const start = new Date();
+            start.setTime(start.getTime() - 3600 * 1000 * 24 * 90);
+            picker.$emit('pick', [start, end]);
+          }
+        }]
+      },
 
-<style lang="scss" scoped></style>
+    }
+  },
+  mounted() {
+    this.handleGetList()
+  },
+  methods: {
+    async handleGetList() {
+      this.parameters.currPage = this.currentPage - 1
+      const { data: res } = await this.axios.get(
+        'http://community.byesame.com/users/getLoginData',
+        {
+          params: this.parameters
+        }
+      )
+      console.log(res)
+      res.data.map((item) => {
+        item.createtime = this.formatCreateTime(item.createtime)
+      })
+      this.tableData = res.data
+      this.total = res.total
+    },
+    // 删除
+    // handleDetele(id) {
+    //   this.$confirm("确定删除该条登录信息？", "提示", {
+    //     confirmButtonText: "确定",
+    //     cancelButtonText: "取消",
+    //     type: "warning",
+    //   }).then(() => {
+    //     this.axios.get('http://community.byesame.com/users/delLoginData',{
+    //       pramas:{
+    //         id:id
+    //       },
+    //       token: sessionStorage.token
+    //     })
+    //     this.$message.success('作废成功')
+    //     this.handleGetList()
+    //   })
+    //   .catch(() => {
+    //       this.$message.error('作废失败')
+    //       this.handleGetList()
+    //     })
+    // },
+
+    search() { },
+
+    handleSizeChange(val) {
+      console.log(`每页 ${val} 条`);
+      this.pageNum = val
+      this.handleGetList()
+    },
+    handleCurrentChange(val) {
+      console.log(`当前页: ${val}`);
+      this.currPage = val
+      this.handleGetList()
+    },
+    // 时间格式化
+    formatCreateTime(dateString) {
+      if (!dateString) return '';
+      const date = new Date(dateString.replace("T", " ").replace("Z", ""));
+      const year = date.getFullYear();
+      const month = String(date.getMonth() + 1).padStart(2, '0');
+      const day = String(date.getDate()).padStart(2, '0');
+      const hours = String(date.getHours()).padStart(2, '0');
+      const minutes = String(date.getMinutes()).padStart(2, '0');
+      const seconds = String(date.getSeconds()).padStart(2, '0');
+      return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+    },
+  },
+}
+</script>
+<style lang="scss" scoped>
+.box::v-deep {
+  .top {
+    display: flex;
+    justify-content: left;
+
+
+  }
+
+  .tab {
+    margin-top: 20px;
+
+    .has-gutter {
+      .el-table__cell {
+        background: rgb(238, 241, 246);
+        border-color: rgb(223, 230, 236);
+        text-align: center;
+        font-size: 14px;
+        color: #000;
+      }
+    }
+
+    .el-table__cell {
+      text-align: center;
+      color: #000;
+      font-size: 14px;
+    }
+  }
+}
+</style>
